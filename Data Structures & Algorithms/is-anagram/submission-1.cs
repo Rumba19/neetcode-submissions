c648bd62f0915beddd7b710f872dc7618916853a
Rumba <brumba30721@ucumberlands.edu>
2026-05-14 14:38:25 -0700

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cs
@@ -0,0 +1,7 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        if(s.Length != t.Length) return false;
+       return s.Order().SequenceEqual(t.Order());
+     
+    }
+}
